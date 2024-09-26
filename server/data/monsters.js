9d430d9412f14c2626ac8abfe42230e89a4fb063
Richard Rodriguez <richardbr.personal@gmail.com>
2024-09-26 00:46:46 -0400

    updated images

--- server/data/monsters.js
+++ server/data/monsters.js
@@ -6,7 +6,7 @@ const monsterData = [
     description:
       "The apex monster of the Ancient Forest, also known as the 'King of the Skies'. A terrible wyvern that can cover the sky in fire.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/0/00/MHRise-Rathalos_Render_001.png/revision/latest?cb=20210107154630",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/rathalos-monster-hunter-world-large-monster.jpg",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const monsterData = [
     description:
       "Known as the 'Tyrant of the Desert'. A menacing, territorial beast that lurks underground.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/3/39/MHRise-Diablos_Render_001.png/revision/latest?cb=20210217223736",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/diablos_monster_hunter_world_large.jpg",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const monsterData = [
     description:
       "Known as the 'Extinction Dragon'. A terrible elder dragon that can regenerate its body at will.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/8/89/MHW-Nergigante_Render_001.png/revision/latest?cb=20190914060417",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/nergigante-gallery.png",
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const monsterData = [
     description:
       "Known as the 'Steel Dragon'. A dragon that can control the wind and rain.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/0/07/MHRise-Kushala_Daora_Render_001.png/revision/latest?cb=20210427142445",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/kushala-daora-large-monster-hunter-world-mhw.jpg",
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const monsterData = [
     description:
       "Known as the 'Flame King Dragon'. A brutal elder dragon that can control fire.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/2/29/MHRise-Teostra_Render_001.png/revision/latest?cb=20210427142335",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/Teostra-large-monster-hunter-world-mhw.jpg",
   },
   {
     id: 6,
@@ -51,7 +51,7 @@ const monsterData = [
     description:
       "Known as the 'Corpse Coat Dragon'. A terrifying elder dragon that can control the effluvium.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/5/54/MHW-Vaal_Hazak_Render_001.png/revision/latest?cb=20180214002433",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/Vaal-Hazak-large-monster-hunter-world-mhw.jpg",
   },
   {
     id: 7,
@@ -60,7 +60,7 @@ const monsterData = [
     description:
       'Fire-breathing female wyverns, also known as the "Queens of the Land". With powerful legs and poison-secreting tails, they hunt mainly on the ground. Sometimes seen preying as a couple, Rathians and Rathalos cooperate well.',
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/b/be/MHRise-Rathian_Render_001.png/revision/latest?cb=20210217224035",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/rathian_transparent.png",
   },
   {
     id: 8,
@@ -69,7 +69,7 @@ const monsterData = [
     description:
       "A monster that must feed constantly and will devour anything in its path, even feeding on the largest of monsters with its massive jowls.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/c/cb/MHW-Deviljho_Render_001.png/revision/latest?cb=20180105132807",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/deviljho-gallery.png",
   },
   {
     id: 9,
@@ -78,7 +78,7 @@ const monsterData = [
     description:
       "Barroths seek out ants, their favorite snack, and mark their territory with mud. They attack potential rivals with their devastating charging attack.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/3/3a/MHRise-Barroth_Render_001.png/revision/latest?cb=20210501170957",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/barroth-large-monster-hunter-world-mhw.jpg",
   },
   {
     id: 10,
@@ -87,7 +87,7 @@ const monsterData = [
     description:
       "Anjanaths are known to inhabit the Ancient Forest and Wildspire Waste. They are known to breathe fire and are very territorial.",
     image:
-      "https://static.wikia.nocookie.net/monsterhunter/images/6/6b/MHRise-Anjanath_Render_001.png/revision/latest?cb=20210308144806",
+      "https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/anjanath-monster-hunter-world-large-monster.jpg",
   },
 ];
 
