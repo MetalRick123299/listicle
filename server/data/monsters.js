09e1b25621e980644c51ccf0b08775c05ac29168
Richard Rodriguez <richardbr.personal@gmail.com>
2024-09-25 22:00:11 -0400

    Added Monster Data

---
+++ server/data/monsters.js
@@ -0,0 +1,92 @@
+const monsterData = [
+  {
+    id: 1,
+    name: "Rathalos",
+    type: "Flying Wyvern",
+    description:
+      "The apex monster of the Ancient Forest, also known as the 'King of the Skies'. A terrible wyvern that can cover the sky in fire.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/0/00/MHRise-Rathalos_Render_001.png/revision/latest?cb=20210107154630",
+  },
+  {
+    id: 2,
+    name: "Diablos",
+    type: "Flying Wyvern",
+    description:
+      "Known as the 'Tyrant of the Desert'. A menacing, territorial beast that lurks underground.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/3/39/MHRise-Diablos_Render_001.png/revision/latest?cb=20210217223736",
+  },
+  {
+    id: 3,
+    name: "Nergigante",
+    type: "Elder Dragon",
+    description:
+      "Known as the 'Extinction Dragon'. A terrible elder dragon that can regenerate its body at will.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/8/89/MHW-Nergigante_Render_001.png/revision/latest?cb=20190914060417",
+  },
+  {
+    id: 4,
+    name: "Kushala Daora",
+    type: "Elder Dragon",
+    description:
+      "Known as the 'Steel Dragon'. A dragon that can control the wind and rain.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/0/07/MHRise-Kushala_Daora_Render_001.png/revision/latest?cb=20210427142445",
+  },
+  {
+    id: 5,
+    name: "Teostra",
+    type: "Elder Dragon",
+    description:
+      "Known as the 'Flame King Dragon'. A brutal elder dragon that can control fire.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/2/29/MHRise-Teostra_Render_001.png/revision/latest?cb=20210427142335",
+  },
+  {
+    id: 6,
+    name: "Vaal Hazak",
+    type: "Elder Dragon",
+    description:
+      "Known as the 'Corpse Coat Dragon'. A terrifying elder dragon that can control the effluvium.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/5/54/MHW-Vaal_Hazak_Render_001.png/revision/latest?cb=20180214002433",
+  },
+  {
+    id: 7,
+    name: "Rathian",
+    type: "Flying Wyvern",
+    description:
+      'Fire-breathing female wyverns, also known as the "Queens of the Land". With powerful legs and poison-secreting tails, they hunt mainly on the ground. Sometimes seen preying as a couple, Rathians and Rathalos cooperate well.',
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/b/be/MHRise-Rathian_Render_001.png/revision/latest?cb=20210217224035",
+  },
+  {
+    id: 8,
+    name: "Deviljho",
+    type: "Brute Wyvern",
+    description:
+      "A monster that must feed constantly and will devour anything in its path, even feeding on the largest of monsters with its massive jowls.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/c/cb/MHW-Deviljho_Render_001.png/revision/latest?cb=20180105132807",
+  },
+  {
+    id: 9,
+    name: "Barroth",
+    type: "Brute Wyvern",
+    description:
+      "Barroths seek out ants, their favorite snack, and mark their territory with mud. They attack potential rivals with their devastating charging attack.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/3/3a/MHRise-Barroth_Render_001.png/revision/latest?cb=20210501170957",
+  },
+  {
+    id: 10,
+    name: "Anjanath",
+    type: "Brute Wyvern",
+    description:
+      "Anjanaths are known to inhabit the Ancient Forest and Wildspire Waste. They are known to breathe fire and are very territorial.",
+    image:
+      "https://static.wikia.nocookie.net/monsterhunter/images/6/6b/MHRise-Anjanath_Render_001.png/revision/latest?cb=20210308144806",
+  },
+];
